c8eb80255d65cbac107003bae1767d9052bb1367
Chris <christopher.martin5555@gmail.com>
2021-12-03 06:19:01 -0800

    Finished the website routes get and post functions

--- Server/routes/websites.js
+++ Server/routes/websites.js
@@ -1,2 +1,49 @@
 const router = require("express").Router();
 const express = require("express");
+const fs = require("fs");
+const uuid = require("uuid");
+
+let websiteList = [];
+
+const grabWebsites = () => {
+  fs.readFile("./data/websites.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    websiteList = JSON.parse(data);
+  });
+};
+
+grabWebsites();
+
+router.get("/", (req, res) => {
+  const strippedData = websiteList.find((website) => {
+    return website.id === req.params.id;
+  });
+  if (!strippedData) {
+    res.status(404).send("Website not found!");
+  }
+  res.json(strippedData);
+});
+
+router.post("/", (req, res) => {
+  const { name, description, link } = req.body;
+
+  const newWebsite = {
+    name,
+    description,
+    link,
+    timestamp: Date.now(),
+    id: uuid(),
+  };
+  websiteList.push(newWebsite);
+
+  fs.writeFile("./data/websites.json", JSON.stringify(websiteList), (err) => {
+    if (err) {
+      res.status(500).send("Post was unsuccessful!");
+    }
+    res.json(websiteList);
+  });
+});
+
+module.exports = router;
