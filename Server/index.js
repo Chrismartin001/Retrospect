00bd8866f6214a891f6b32a383ec27224abf3e86
Chris <christopher.martin5555@gmail.com>
2021-12-05 07:15:28 -0800

    Added server side jwt token authentication in index.js

--- Server/index.js
+++ Server/index.js
@@ -11,6 +11,47 @@ app.use(morgan("dev"));
 app.use("/api/v1/reviews", reviewRoutes);
 app.use("/api/v1/websites", websiteRoutes);
 
+// app.use(express.static(path.join(__dirname, "public")));
+function authorize(req, res, next) {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(403).json({ Message: "No token. authorized access! " });
+  }
+  if (jwt.verify(token, secret)) {
+    req.decoded = jwt.decode(token);
+    console.log(req.decoded);
+    next;
+  }
+}
+
+const users = {};
+
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  const user = (users[username] = {
+    name,
+    password,
+  });
+  return res.json({ success: "true" });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users[username];
+  if (user && user.password === password) {
+    const token = jwt.sign({ name: user.name });
+    return res.status(200).json({ token });
+  }
+  return res.status(500).json({ message: "ERROR MESSAGE! " });
+});
+
+app.get("/profile", authorize, (req, res) => {
+  console.log(req.decoded);
+  res.json(req.decoded);
+});
+
 app.listen(PORT, () => {
   console.log(`Express Server is running on PORT http://localhost:${PORT}`);
 });
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <p>Hello</p>
+      <h1>RetroSpeckt</h1>
+      <ul>
+        <li>Home</li>
+        <li>login</li>
+      </ul>
     </div>
   );
 }
