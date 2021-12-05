7a51dde34fe048ae54f814355a14204ee4c0e471
Chris <christopher.martin5555@gmail.com>
2021-12-05 12:23:04 -0800

    Added Routes in App.js and installed bootstrap to help with Sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route
+              path="/websites/:id"
+              render={(...reactRouterDomProps) => {
+                return <Home {...reactRouterDomProps} />;
+              }}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Navbar.scss";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="navbar-nav">
       <h1>RetroSpeckt</h1>
       <ul>
-        <li>Home</li>
-        <li>login</li>
+        <li className="nav-link">Home</li>
+        <li className="nav-link">login</li>
       </ul>
     </div>
   );
