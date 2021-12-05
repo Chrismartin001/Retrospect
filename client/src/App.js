b861cee21510545d00131855c3bdd3e912c38eea
Chris <christopher.martin5555@gmail.com>
2021-12-05 13:20:16 -0800

    Setting up login and sign up functions and added some Link routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,75 @@ import "./App.scss";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 class App extends Component {
+  state = {
+    isSignedUp: false,
+    isLoggedIn: false,
+    isLoginError: false,
+    errorMessage: "",
+  };
+
+  signup = (e) => {
+    e.preventDefault();
+    const { username, name, password } = this.signUpForm;
+  };
+
+  login = (e) => {
+    e.preventDefault();
+    const { username, password } = this.loginForm;
+  };
+
+  renderSignUp() {
+    return (
+      <div>
+        <h1>SignUp</h1>
+        <form ref={(form) => (this.signUpForm = form)}>
+          <div className="form-group">
+            Username: <input type="text" name="username" />
+          </div>
+          <div className="form-group">
+            Name: <input type="text" name="name" />
+          </div>
+          <div className="form-group">
+            Password: <input type="password" name="password" />
+          </div>
+          <button className="btn btn-primary" onClick={this.signup}>
+            Signup
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  renderLogin = () => {
+    const { isLoginError, errorMessage } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        {isLoginError && <label style={{ color: "red" }}>{errorMessage}</label>}
+        <form ref={(form) => (this.loginForm = form)}>
+          <div className="form-group">
+            Username: <input type="text" name="username" />
+          </div>
+          <div className="form-group">
+            Password: <input type="password" name="password" />
+          </div>
+          <button className="btn btn-primary" onClick={this.login}>
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  };
+
   render() {
+    const { isLoggedIn, isSignedUp } = this.state;
+
+    if (!isSignedUp) return this.renderSignUp();
+    if (!isLoggedIn) return this.renderLogin();
+
     return (
       <div>
         <BrowserRouter>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar-nav">
-      <h1>RetroSpeckt</h1>
+      <Link to="/">
+        <h1>RetroSpeckt</h1>
+      </Link>
       <ul>
-        <li className="nav-link">Home</li>
-        <li className="nav-link">login</li>
+        <Link to="/">
+          <li className="nav-link">Home</li>
+        </Link>
+        <Link to="/">
+          <li className="nav-link">login</li>
+        </Link>
       </ul>
     </div>
   );
